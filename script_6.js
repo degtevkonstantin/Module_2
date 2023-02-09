2d7c16c2835de77f95624666f2b9f17eb7f51825
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-09 16:05:25 +0300

    Add files via upload

---
+++ script_6.js
@@ -0,0 +1,40 @@
+// Напишите функцию, которая сгруппирует объекты заказов по имени.
+
+// Input: [
+//   { name: "test", price: 200 },
+//   { name: "test1", price: 300 },
+//   { name: "test", price: 100 },
+//   { name: "test", price: 600 }
+// ]
+
+// Output: [
+//   { name: "test", price: 900 },
+//   { name: "test1", price: 300 }
+// ]
+
+const formByName = (array) => {
+  let resultArray = [];
+  let counter = 0;
+
+  for (element1 of array) {
+    for (element2 of resultArray) {
+      if (element2.name === element1.name) {
+        element2.price += element1.price
+        counter++
+      }
+    }
+
+    if (counter == 0) {
+      resultArray.push(element1);
+    }
+  }
+
+  return resultArray;
+}
+
+console.log(formByName([
+  { name: "test", price: 200 },
+  { name: "test1", price: 300 },
+  { name: "test", price: 100 },
+  { name: "test", price: 600 },
+]));
