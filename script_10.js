8061f2cf260d17a62cba95ada9210f1eef2006c2
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-13 09:52:56 +0300

    Add files via upload

--- script_10.js
+++ script_10.js
@@ -4,14 +4,14 @@
 
 // Output: 6
 
-const frequentElement = (array) => {
+const oftenElement = (array) => {
   let counter = 0;
   let maxCounter = 0;
   let maxElement = 0;
   
-  for (element1 of array) {
+  for (const element1 of array) {
     counter = 0;
-    for (element2 of array) {
+    for (const element2 of array) {
       if (element1 == element2) {
         counter++;
       };
@@ -26,4 +26,4 @@ const frequentElement = (array) => {
   return maxElement;
 }
 
-console.log(frequentElement([6, 3, 8, 2, 6, 8, 2, 5, 7, 2, 6, 8, 6, 2, 6]));
+console.log(oftenElement([6, 3, 8, 2, 6, 8, 2, 5, 7, 2, 6, 8, 6, 2, 6]));
