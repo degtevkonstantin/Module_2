1d2d7f15745a4ed0300936313dc6e502ad022f29
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-13 10:00:37 +0300

    Add files via upload

--- script_13.js
+++ script_13.js
@@ -8,22 +8,18 @@
 //   "1": 8
 // }
 
-const countElements = (array) => {
+const counterElements = (array) => {
   let count0 = 0;
   let count1 = 0;
-  
-  for (element of array) {
-    if (element == 0){
-      count0++;
-    }else {
-      count1++;
-    }
-  }
-  
+
+  for (const element of array) {
+    element == 0 ? count0++ : count1++;
+  };
+
   return {
     '0': count0,
     '1': count1
   }
 }
 
-console.log(countElements([0, 0, 0, 1, 1, 0 , 1, 1, 1, 0, 0, 1, 1, 0, 1]));
+console.log(counterElements([0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1]));
