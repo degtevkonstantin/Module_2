c5680ad34f4db6f5883b0fce8f68621419d17e60
degtevkonstantin <123629309+degtevkonstantin@users.noreply.github.com>
2023-02-14 10:11:54 +0300

    Add files via upload

--- script_12.js
+++ script_12.js
@@ -4,32 +4,32 @@
 
 // Output: ["2", "part", "test", "education", "exceed.team"]
 
-const formArray = (array) => {
+const shapeArray = (array) => {
   const countArray = [];
   const resultArray = [];
 
   for (const element of array){
     countArray.push(element.length)
-  }
+  };
 
   let counter1 = 0;
-  for (const element of array) {
+  for (let counter3 = 0; counter3 < array.length; counter3++) {
     for (const index in countArray) {
       counter1 = 0;
       for (const element2 of countArray) {
         if (countArray[index] < element2) {
-          counter1++
-        }
-      }
+          counter1++;
+        };
+      };
       if (counter1 == 0) {
-        resultArray.push(array[index])
+        resultArray.push(array[index]);
         delete array[index];
         delete countArray[index];
-      }
-    }
-  }
+      };
+    };
+  };
 
-  return resultArray
+  return resultArray;
 }
 
-console.log(formArray(["test", "education", "part", "2", "exceed.team", 'qw']));
+console.log(shapeArray(["test", "education", "part", "2", "exceed.team", 'qw']));
